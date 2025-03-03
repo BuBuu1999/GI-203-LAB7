855989882b0722416677ad951457b8f5d059bc02
BuBuu1999 <buranapohrn.jaiy@bumail.net>
2025-03-03 11:09:00 +0700

    Stage4

--- Assets/Codes/Airplane.cs
+++ Assets/Codes/Airplane.cs
@@ -7,16 +7,19 @@ public class Airplane : MonoBehaviour
     [SerializeField] float liftBooster = 0.5f;
     [SerializeField] float drag = 0.001f;
     [SerializeField] float angularDrag = 0.001f;
-    
-   
+    [SerializeField] float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;    
+    [SerializeField] float rollPower = 30f;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    
     void FixedUpdate()
     {
         
     }
+    
+    
 }
